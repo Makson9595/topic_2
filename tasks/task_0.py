90f9ce3e23198979eedf3d53fb8414252f35860e
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-03 16:47:46 +0000

    Проверка ДЗ
    - Правильные: №0, №1, №2, №3, №4
    - Неправильные: №5,
    - Улучшены: №0, №1
    - На доработку: №5,

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,5 +1,7 @@
+# №1
 print("Hello, world!", "Hello, world!", "Hello, world!", "Hello, world!",
 "Hello, world!","Hello, world!", "Hello, world!", "Hello, world!", "Hello, world!",
 "Hello, world!", sep='\n')
 
-
+# №2
+print("Hello, world!\n" * 10)
--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,6 +1,8 @@
 price = 50 # цена за 1шт
 quantity = 3 # кол-во яиц
 
-total_cost = "Вы должны заплатить " + str(price * quantity) + " рублей за " + str(quantity) + " единиц товара по цене " + str(price) + " рублей за единицу"
+total_cost = ("Вы должны заплатить " + str(price * quantity)
+              + " рублей за " + str(quantity) + " единиц товара по цене "
+              + str(price) + " рублей за единицу")
 
 print(total_cost)

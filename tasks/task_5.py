d425b499ea68073f9862204bb3a777b2d6d6567a
Bill Williamson <vkontakte-maksimka@mail.ru>
2025-03-07 16:59:30 +0300

    ТЕМА №2 -Задание №5

--- tasks/task_5.py
+++ tasks/task_5.py
@@ -1,7 +1,7 @@
 weight_in_grams = 350000  # гр
 
 weight_in_kilograms = weight_in_grams / 1000  # кг
-weight_in_tons = weight_in_grams / 10000  # тонны
+weight_in_tons = weight_in_grams / 1000000  # тонны
 
 print(weight_in_kilograms)
 print(weight_in_tons)
